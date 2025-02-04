64632e5bd1edcfdd052960855d44b95abd790686
Siddhartha Khanal <sidkhanal77@gmail.com>
2025-02-04 14:48:45 +0545

    fix

--- src/routes/api/series/watchlist/get/+server.js
+++ src/routes/api/series/watchlist/get/+server.js
@@ -4,20 +4,16 @@ import PocketBase from 'pocketbase';
 
 
 export async function POST({ request, cookies }) {
-    const pb = new PocketBase(POCKETBASE_URL);
-
     try {
+        const pb = new PocketBase(POCKETBASE_URL);
         const { user_id, tmdb_id } = await request.json();
         const userIdString = user_id.toString();
         const resultList = await pb.collection('favourite_series').getList(1, 50, {
             filter: `user_id="${userIdString}"`,
         });
         const result = await pb.collection('favourite_series').getFirstListItem(`tmdb_id="${tmdb_id}"`)
-
         return json({ records: result }, { status: 200 });
-
     } catch (error) {
-
         console.error('Error fetching favourite series:', error);
         return json({ error: error.message || 'An error occurred' }, { status: 500 });
     }
